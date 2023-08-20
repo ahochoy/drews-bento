68b8b335be180a7b819807b57f9ac4601e043df2
Andrew HoChoy <a.hochoy@gmail.com>
2023-08-20 00:37:19 -0700

    Basic layout and componentry

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
-
 import netlify from "@astrojs/netlify/functions";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: netlify()
+  adapter: netlify(),
+  integrations: [react(), tailwind(), partytown()]
 });
